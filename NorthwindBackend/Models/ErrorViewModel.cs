7fba635a6eba31c0f9c1ead0fce6ca099ffd7ae1
ozbek <ozbek@DESKTOP-LARM3NL>
2023-07-24 17:50:00 +0300

    Add project files.

---
+++ NorthwindBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NorthwindBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
